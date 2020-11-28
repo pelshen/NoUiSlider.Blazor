cca7756e8de3c7782eb71e19a62dc2dcc6e8102e
Patrick Elshen <patrick@elshens.uk>
2020-11-28 09:20:44 +0000

    Add sample project
    Add sample project for testing and demo

---
+++ PElshen.NoUiSlider.Blazor.Sample/Pages/Index.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PElshen.NoUiSlider.Blazor.Sample.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        private double ValueBasic { get; set; } = 10;
+
+        private bool IsDisabled { get; set; } = false;
+
+        private string BasicExample { get; set; } = "<NoUiSlider @bind-Value=\"Value\" Min=\"0\" Max=\"100\" Tooltips=\"true\" />";
+
+        private string DisableExample { get; set; } = @"<NoUiSlider @bind-Value=""ValueBasic"" Min=""0"" Max=""100"" Tooltips=""true"" IsDisabled=""IsDisabled"" />
+<button @onclick=""ToggleDisabled"">@(IsDisabled ? ""Enable"" : ""Disable"")</button>
+
+private void ToggleDisabled()
+{
+    IsDisabled = !IsDisabled;
+}";
+
+        private void ToggleDisabled()
+        {
+            IsDisabled = !IsDisabled;
+        }
+    }
+}
